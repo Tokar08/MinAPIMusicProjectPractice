0e35b287b391febb8f48afdbc63af23d9f4c7e8f
Tokar08 <ivan.tokar.git@gmail.com>
2024-08-21 20:57:47 +0300

    Initial commit

---
+++ DTOs/AddTrackDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MinAPIMusicProject.DTOs;
+
+public class AddTrackDTO
+{
+    public string Title { get; set; }
+    public int DurationInSeconds { get; set; }
+    public int GenreId { get; set; }
+}
